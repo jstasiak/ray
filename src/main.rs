5e4da72501d6fe00126897b5cb80fefdb4c38c7d
Jakub Stasiak <jakub@stasiak.at>
2019-07-06 00:15:22 +0200

    Move writing PPM to a separate function

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,79 @@ fn main() {
         _ => Box::new(File::create(filename).expect("Cannot open file for writing")),
     };
 
-    render(&mut file, 200, 100);
+    let image = render(200, 100);
+    image_to_file(&image, &mut file);
 }
 
-fn render(w: &mut Write, width: isize, height: isize) {
-    write!(w, "P3\n{} {}\n255\n", width, height).expect("Cannot write");
+fn render(width: usize, height: usize) -> Image {
+    Image::new(width, height)
+}
 
-    for _ in 0..width {
-        for _ in 0..height {
-            write!(w, "100 0 0 ").expect("Cannot write");
+fn image_to_file(image: &Image, w: &mut Write) {
+    write!(w, "P3\n{} {}\n255\n", image.width(), image.height()).expect("Cannot write");
+
+    for x in 0..image.width() {
+        for y in 0..image.height() {
+            let color = image.get_color(x, y);
+            write!(
+                w,
+                "{} {} {} ",
+                (color.r * 255.0) as u8,
+                (color.g * 255.0) as u8,
+                (color.b * 255.0) as u8
+            )
+            .expect("Cannot write");
         }
         write!(w, "\n").expect("Cannot write");
     }
     w.flush().expect("Cannot flush");
+}
+
+struct Image {
+    buffer: Vec<Color>,
+    w: usize,
+    h: usize,
+}
+
+impl Image {
+    fn new(width: usize, height: usize) -> Image {
+        Image {
+            buffer: vec![Color::new_black(); width * height],
+            w: width,
+            h: height,
+        }
+    }
+
+    fn set_color(&mut self, x: usize, y: usize, color: Color) {
+        self.buffer[y * self.w + x] = color;
+    }
+
+    fn get_color(&self, x: usize, y: usize) -> Color {
+        self.buffer[y * self.w + x]
+    }
+
+    fn width(&self) -> usize {
+        self.w
+    }
+
+    fn height(&self) -> usize {
+        self.h
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
+impl Color {
+    fn new(r: f32, g: f32, b: f32) -> Color {
+        Color { r, g, b }
+    }
+
+    fn new_black() -> Color {
+        Self::new(0.0, 0.0, 0.0)
+    }
 }
